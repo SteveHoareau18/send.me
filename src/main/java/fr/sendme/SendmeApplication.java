34040a63c73ee69fe6355ad05f019a4b687dd342
Steve <steve.hoareau1@gmail.com>
2024-11-08 12:06:30 +0100

    init

---
+++ src/main/java/fr/sendme/SendmeApplication.java
@@ -0,0 +1,13 @@
+package fr.sendme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendmeApplication.class, args);
+    }
+
+}
