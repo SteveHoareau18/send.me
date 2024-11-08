9c88325985f677cdc147e215826dc73882eee989
Steve <steve.hoareau1@gmail.com>
2024-11-08 15:30:15 +0100

    init

---
+++ sendme_back/src/main/java/fr/sendme/SendmeApplication.java
@@ -0,0 +1,12 @@
+package src.main.java.fr.sendme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendmeApplication.class, args);
+    }
+}
---
+++ sendme_back/src/test/java/fr/sendme/SendmeApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.fr.sendme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
