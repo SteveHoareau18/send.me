200ff0746855ee7e618106dfec258e026b74535e
SIZUUKA <sfnour3@gmail.Com>
2024-12-16 11:00:55 +0100

    work on front based on preority task (home page, login , upload )

--- sendme_front/src/App.tsx
+++ sendme_front/src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+            <header className="text-center">
+              <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
+                send.me
+              </h1>
+              <h2 className="text-4xl md:text-6xl font-bold mb-4">
+                Partagez vos fichiers en toute simplicité
+              </h2>
+              <p className="text-lg md:text-xl font-light mb-8">
+                Envoyez des fichiers rapidement et de manière sécurisée avec notre plateforme.
+              </p>
+              <a
+                href="/login"
+                className="px-6 mr-2 py-3 bg-white text-indigo-600 font-medium text-lg rounded-lg shadow-md hover:bg-gray-100 transition duration-300"
+              >
+                Connexion
+              </a>
+              <a
+                href="/register"
+                className="px-6 py-3 bg-white text-indigo-600 font-medium text-lg rounded-lg shadow-md hover:bg-gray-100 transition duration-300"
+              >
+                S'inscrire
+              </a>
+            </header>
+          </div>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
