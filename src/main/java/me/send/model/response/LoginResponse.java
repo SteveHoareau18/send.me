54db677295dd2add3836f79af590ddda2c77a3b6
SteveHoareau18 <steve.hoareau1@gmail.com>
2024-12-20 13:27:00 +0000

    [feature] move to gradle

---
+++ src/main/java/me/send/model/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package me.send.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponse {
+    private String token;
+    private long expiresIn;
+}
